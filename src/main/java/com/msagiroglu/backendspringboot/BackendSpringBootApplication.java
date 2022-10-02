02a1564edc2bed47d24a2a2b4a0f590be7ea89b8
BuildTools <m.sagiroglu@outlook.de>
2022-10-02 18:13:30 +0200

    set up mysql remote connection

--- src/main/java/com/msagiroglu/backendspringboot/BackendSpringBootApplication.java
+++ src/main/java/com/msagiroglu/backendspringboot/BackendSpringBootApplication.java
@@ -1,10 +1,11 @@
 package com.msagiroglu.backendspringboot;
 
-import com.msagiroglu.backendspringboot.model.Blog;
 import com.msagiroglu.backendspringboot.model.Role;
 import com.msagiroglu.backendspringboot.model.User;
-import com.msagiroglu.backendspringboot.service.BlogService;
+import com.msagiroglu.backendspringboot.repo.RoleRepo;
+import com.msagiroglu.backendspringboot.repo.UserRepo;
 import com.msagiroglu.backendspringboot.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,37 +13,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
 public class BackendSpringBootApplication {
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendSpringBootApplication.class, args);
     }
 
     @Bean
-    CommandLineRunner run(UserService userService, BlogService blogService){
+    CommandLineRunner run(UserService userService){
         return args -> {
-            userService.saveRole(new Role(null, "ROLE_USER"));
-            userService.saveRole(new Role(null, "ROLE_ADMIN"));
 
-            userService.saveUser(new User(null, "user1", "user1@user1.com", "12345", new ArrayList<>()));
-            userService.saveUser(new User(null, "user2", "user2@user2.com", "12345", new ArrayList<>()));
-            userService.saveUser(new User(null, "user3", "user3@user3.com", "12345", new ArrayList<>()));
-            userService.saveUser(new User(null, "user4", "user4@user4.com", "12345", new ArrayList<>()));
+            if (!roleRepo.existsByName("ROLE_USER") && !roleRepo.existsByName("ROLE_ADMIN")) {
+                userService.saveRole(new Role(null, "ROLE_USER"));
+                userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            }
 
-            userService.addRoleToUser("user1", "ROLE_USER");
-            userService.addRoleToUser("user1", "ROLE_ADMIN");
-            userService.addRoleToUser("user2", "ROLE_USER");
-            userService.addRoleToUser("user3", "ROLE_ADMIN");
-            userService.addRoleToUser("user4", "ROLE_ADMIN");
-
-            blogService.saveBlog(new Blog(null, "My first Blog entry", "My sub-titel for this entry", "Here i can write everything i 1want", new Date()));
-            blogService.saveBlog(new Blog(null, "My second Blog entry", "My sub-titel for this entry", "Here i can write everything i 2want", new Date()));
-            blogService.saveBlog(new Blog(null, "My third Blog entry", "My sub-titel for this entry", "Here i can write everything i 3want", new Date()));
-            blogService.saveBlog(new Blog(null, "My fourth Blog entry", "My sub-titel for this entry", "Here i can write everything i 4want", new Date()));
+            if (!userRepo.existsByUsername("user1")) {
+                userService.saveUser(new User(null, "user1", "user1@user1.com", System.getenv("USER_PASS"), new ArrayList<>()));
+                userService.addRoleToUser("user1", "ROLE_USER");
+                userService.addRoleToUser("user1", "ROLE_ADMIN");
+            }
 
         };
     }
--- src/main/java/com/msagiroglu/backendspringboot/repo/UserRepo.java
+++ src/main/java/com/msagiroglu/backendspringboot/repo/UserRepo.java
@@ -9,4 +9,5 @@ public interface UserRepo extends JpaRepository<User, Long> {
     void deleteUserById(Long id);
     Optional<User> findUserById(Long id);
     Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
